6ce08a84c4e80cf4c6d062b9ad7341075ccc55a0
Mohsin Shakeel <mohsinmorosoft@gmail.com>
2025-02-21 11:41:09 +0500

    Readme File Created...

---
+++ zoho-integration.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Plugin Name:       Custom Zoho Integration
+ * Description:       Send custom data to zoho sheet & updates rows based on wordpress updates.
+ * Version:           1.0.0
+ * Author:            Mohsin Shakeel
+ */
